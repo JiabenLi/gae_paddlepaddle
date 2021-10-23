d13ac632df7350796eab88b3d3ab0d492e836a6c
JiabenLi <55351412+JiabenLi@users.noreply.github.com>
2021-10-23 10:28:52 +0800

    Add files via upload

---
+++ gae/model.py
@@ -0,0 +1,45 @@
+import paddle
+import paddle.nn.functional as F
+import paddle.nn as nn
+
+from gae.layers import GraphConvolution
+
+
+class GCNModelVAE(nn.Layer):
+    def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout):
+        super(GCNModelVAE, self).__init__()
+        self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
+        self.gc2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+        self.gc3 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
+        self.dc = InnerProductDecoder(dropout, act=lambda x: x)
+
+    def encode(self, x, adj):
+        hidden1 = self.gc1(x, adj)
+        return self.gc2(hidden1, adj), self.gc3(hidden1, adj)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = paddle.exp(logvar)
+            eps = paddle.randn(std.shape)
+            return eps.multiply(std).add(mu)
+        else:
+            return mu
+
+    def forward(self, x, adj):
+        mu, logvar = self.encode(x, adj)
+        z = self.reparameterize(mu, logvar)
+        return self.dc(z), mu, logvar
+
+
+class InnerProductDecoder(nn.Layer):
+    """Decoder for using inner product for prediction."""
+
+    def __init__(self, dropout, act=F.sigmoid):
+        super(InnerProductDecoder, self).__init__()
+        self.dropout = dropout
+        self.act = act
+
+    def forward(self, z):
+        z = F.dropout(z, self.dropout, training=self.training)
+        adj = self.act(paddle.matmul(z, z.t()))
+        return adj
