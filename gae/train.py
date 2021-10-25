fcb8aa51b9c87f3bd47ee71ebae3d6d23d5bea7a
JiabenLi <55351412+JiabenLi@users.noreply.github.com>
2021-10-25 18:22:22 +0800

    Add files via upload

---
+++ gae/train.py
@@ -0,0 +1,90 @@
+# from __future__ import division
+# from __future__ import print_function
+
+import argparse
+import time
+
+import numpy as np
+import scipy.sparse as sp
+import paddle
+from paddle import optimizer
+
+from gae.model import GCNModelVAE
+from gae.optimizer import loss_function
+from gae.utils import load_data, mask_test_edges, preprocess_graph, get_roc_score
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='gcn_vae', help="models used")
+parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=200, help='Number of epochs to train.')
+parser.add_argument('--hidden1', type=int, default=32, help='Number of units in hidden layer 1.')
+parser.add_argument('--hidden2', type=int, default=16, help='Number of units in hidden layer 2.')
+parser.add_argument('--lr', type=float, default=0.01, help='Initial learning rate.')
+parser.add_argument('--dropout', type=float, default=0., help='Dropout rate (1 - keep probability).')
+parser.add_argument('--dataset-str', type=str, default='citeseer', help='type of dataset.')
+
+args = parser.parse_args()
+
+
+def gae_for(args):
+    print("Using {} dataset".format(args.dataset_str))
+    adj, features = load_data(args.dataset_str)
+    n_nodes, feat_dim = features.shape
+
+    # Store original adjacency matrix (without diagonal entries) for later
+    adj_orig = adj
+    adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
+    adj_orig.eliminate_zeros()
+
+    adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = mask_test_edges(adj)
+    adj = adj_train
+
+    # Some preprocessing
+    adj_norm = preprocess_graph(adj)
+    adj_label = adj_train + sp.eye(adj_train.shape[0])
+    # adj_label = sparse_to_tuple(adj_label)
+    adj_label = paddle.to_tensor(adj_label.toarray(), dtype='float32')
+
+    pos_weight = float(adj.shape[0] * adj.shape[0] - adj.sum()) / adj.sum()
+    pos_weight = paddle.to_tensor(pos_weight)
+    norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.sum()) * 2)
+
+    model = GCNModelVAE(feat_dim, args.hidden1, args.hidden2, args.dropout)
+    # model_optimizer = optimizer.Adam(learning_rate=args.lr, parameters=model.parameters())
+    #
+    # hidden_emb = None
+    # for epoch in range(args.epochs):
+    #     t = time.time()
+    #     model.train()
+    #     model_optimizer.clear_grad()
+    #
+    #     recovered, mu, logvar = model(features, adj_norm)
+    #     loss = loss_function(preds=recovered, labels=adj_label,
+    #                          mu=mu, logvar=logvar, n_nodes=n_nodes,
+    #                          norm=norm, pos_weight=pos_weight)
+    #     loss.backward()
+    #     cur_loss = loss.item()
+    #     model_optimizer.step()
+    #
+    #     hidden_emb = mu.numpy()
+    #     roc_curr, ap_curr = get_roc_score(hidden_emb, adj_orig, val_edges, val_edges_false)
+    #
+    #     print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(cur_loss),
+    #           "val_ap=", "{:.5f}".format(ap_curr),
+    #           "time=", "{:.5f}".format(time.time() - t)
+    #           )
+    #
+    # print("Optimization Finished!")
+    # paddle.save(model.state_dict(), 'gae_paddlepaddle.pdparams')
+
+    model.load_dict(paddle.load('gae_paddlepaddle.pdparams'))
+    recovered, mu, logvar = model(features, adj_norm)
+    hidden_emb = mu.numpy()
+
+    roc_score, ap_score = get_roc_score(hidden_emb, adj_orig, test_edges, test_edges_false)
+    print('Test ROC score: ' + str(roc_score))
+    print('Test AP score: ' + str(ap_score))
+
+
+if __name__ == '__main__':
+    gae_for(args)
